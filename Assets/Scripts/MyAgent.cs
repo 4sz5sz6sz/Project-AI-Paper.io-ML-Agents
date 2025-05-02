652709f9e7358e926026da1355217f32b62c2698
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2025-05-02 18:09:38 +0900

    기본 조작 세팅 (WIP)

--- Assets/Scripts/MyAgent.cs
+++ Assets/Scripts/MyAgent.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using MLAgents;
 
-public class MyAgents : MonoBehaviour
+public class MyAgent : Agent
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // 에피소드가 시작될 때 호출됩니다. 초기화가 필요한 경우 이곳에서 처리합니다.
+    public override void OnEpisodeBegin()
     {
-        
+        // 1. 플레이어의 위치 초기화
+        // 2. AI의 초기 위치 설정
+        // 3. 게임 맵 리셋 (전략적인 맵 초기화)
+        // 예시: 플레이어와 AI의 시작 위치를 랜덤하게 설정하고, 맵을 초기화.
     }
 
-    // Update is called once per frame
-    void Update()
+    // 에이전트가 환경에 대해 관찰하는 데이터를 수집하는 곳입니다.
+    public override void CollectObservations(VectorSensor sensor)
     {
-        
+        // 1. 플레이어의 현재 위치
+        // 2. AI의 위치
+        // 3. 점령된 영역 정보
+        // 4. 장애물이나 상대방의 위치
+        // 예시: 플레이어의 좌표, AI의 좌표, 점령한 땅의 영역을 센서에 추가.
+    }
+
+    // 에이전트가 선택한 행동을 기반으로 보상을 계산하거나 환경을 업데이트하는 곳입니다.
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        // 1. 행동에 따른 플레이어 이동 (상, 하, 좌, 우 등)
+        // 2. 플레이어가 점령한 영역을 업데이트
+        // 3. 점령된 땅에 대한 보상 처리
+        // 예시: 행동에 따라 플레이어 이동, 점령한 땅의 면적에 따른 보상 제공.
+    }
+
+    // 수동으로 행동을 테스트하고 싶을 때 정의할 수 있습니다.
+    public override void Heuristic(float[] actionsOut)
+    {
+        // 1. 키보드나 마우스를 이용해 수동으로 행동 결정
+        // 2. 상, 하, 좌, 우 이동 조작
+        // 예시: WASD 키로 플레이어 이동, 마우스 클릭으로 AI의 행동 조작.
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // 방향키 입력 받기 (상, 하, 좌, 우)
+        float moveX = Input.GetAxis("Horizontal");  // 좌우 (A, D or Left Arrow, Right Arrow)
+        float moveY = Input.GetAxis("Vertical");    // 상하 (W, S or Up Arrow, Down Arrow)
+
+        // 방향키에 따라 빨간 네모 이동
+        Vector3 move = new Vector3(moveX, moveY, 0f) * moveSpeed * Time.deltaTime;
+        transform.Translate(move);
     }
 }
