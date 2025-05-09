0f49221c018f5f476a90aa50bbd3f6d86ccb095f
4sz5sz6sz <98520968+4sz5sz6sz@users.noreply.github.com>
2025-05-09 19:44:41 +0900

    조작법 개선, 가장 마지막으로 입력된 방향으로 쭉 나아감.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    //가장 마지막으로 입력된 방향 저장.    
+    private Vector2 currentDirection = Vector2.zero;
+
 
     void Update()
     {
-        // 방향키 입력 받기 (상, 하, 좌, 우)
-        float moveX = Input.GetAxis("Horizontal");  // 좌우 (A, D or Left Arrow, Right Arrow)
-        float moveY = Input.GetAxis("Vertical");    // 상하 (W, S or Up Arrow, Down Arrow)
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+            currentDirection = Vector2.up;
+        }
+        else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+            currentDirection = Vector2.down;
+        }
+        else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+            currentDirection = Vector2.left;
+        }
+        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            currentDirection = Vector2.right;
+        }
 
         // 방향키에 따라 빨간 네모 이동
-        Vector3 move = new Vector3(moveX, moveY, 0f) * moveSpeed * Time.deltaTime;
+        Vector3 move = new Vector3(currentDirection.x, currentDirection.y, 0f) * moveSpeed * Time.deltaTime;
         transform.Translate(move);
     }
 }
